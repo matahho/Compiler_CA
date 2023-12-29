d25d60c3d41d859f424af4cae39efde5cd1847f4
notarealone <alireza.hosseini.81@gmail.com>
2023-12-29 18:57:19 +0330

    same as last commit

--- src/main/ast/type/complexType/CandleType.java
+++ src/main/ast/type/complexType/CandleType.java
@@ -3,4 +3,8 @@ package main.ast.type.complexType;
 import main.ast.type.Type;
 
 public class CandleType extends Type {
+    @Override
+    public String toString() {
+        return "Candle";
+    }
 }
--- src/main/ast/type/complexType/TradeType.java
+++ src/main/ast/type/complexType/TradeType.java
@@ -3,4 +3,8 @@ package main.ast.type.complexType;
 import main.ast.type.Type;
 
 public class TradeType extends Type {
+    @Override
+    public String toString() {
+        return "Trade";
+    }
 }
